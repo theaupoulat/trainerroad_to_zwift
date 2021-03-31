43212709511c74f62b1cce926563010153b35c19
Theau <Poulat>
2021-04-01 01:48:09 +0200

    feat: add button to calendar preview and some refactor

--- buttonScript.js
+++ buttonScript.js
@@ -1,34 +1,107 @@
-setTimeout(function () {
-  const scheduleButton = document.getElementById("workoutOverviewStats");
-  console.log(scheduleButton);
+setInterval(function () {
+  if (
+    document.getElementById("workoutOverviewStats") &&
+    !document.getElementById("zwiftButton")
+  ) {
+    addZwiftExportButton("workoutOverviewStats");
+  } else if (
+    document.getElementById("add") &&
+    !document.getElementById("zwiftButton")
+  ) {
+    addZwiftExportButton("add");
+  }
+}, 500);
+
+function addZwiftExportButton(siblingId) {
+  const scheduleButton = document.getElementById(siblingId);
+  const zwiftButton = createZwiftButton();
+  let parentDiv = scheduleButton.parentNode;
+  parentDiv.insertBefore(zwiftButton, scheduleButton.nextSibling);
+}
+
+function createZwiftButton() {
   let zwiftButton = document.createElement("button");
   zwiftButton.onclick = getWorkoutData;
   zwiftButton.innerHTML = "Export to Zwift";
-  console.log(scheduleButton);
-  let parentDiv = scheduleButton.parentNode;
+  zwiftButton.setAttribute("id", "zwiftButton");
+  zwiftButton.classList.add(
+    "Button__button___3ip3e",
+    "Button__button--blue___2e46W",
+    "Button__button--small___rtJlF"
+  );
+  zwiftButton.style.marginLeft = "5px";
 
-  parentDiv.insertBefore(zwiftButton, scheduleButton.nextSibling);
-}, 4000);
+  return zwiftButton;
+}
+
+function getWorkoutId() {
+  let workoutId;
+  const isWorkoutPage = document.getElementById("workoutOverviewStats");
+  const isCalendarModal = document.getElementById("add");
+  if (isWorkoutPage) {
+    const pageUrl = window.location.href;
+    const workoutDenominator = pageUrl.replace(
+      "https://www.trainerroad.com/app/cycling/workouts/",
+      ""
+    );
+    workoutId = workoutDenominator.split("-")[0];
+  } else if (isCalendarModal) {
+    let workout = document.getElementById("mp__calendar--modal-workout-card");
+    const workoutImage =
+      workout.children[0].children[0].children[0].children[0];
+    let workoutImageSource = workoutImage.src;
+    let splitted = workoutImageSource.split("/");
+    workoutId = splitted[splitted.indexOf("workouts") + 1];
+  }
+  return workoutId;
+}
+
+function exportToXml(workoutName, workoutString) {
+  const xml = `
+  <workout_file>
+    <author/>
+    <name>${workoutName}</name>
+    <tags>
+      <tag name="TrainerRoad"/>
+    </tags>
+    <description/>
+    <sportType>bike</sportType>
+    <durationType>time</durationType>
+    <tags/>
+    <workout>
+      ${workoutString}
+    </workout>
+  </workout_file>
+  `;
+
+  const filename = workoutName + ".zwo";
+
+  var element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(xml)
+  );
+  element.setAttribute("download", filename);
+
+  element.style.display = "none";
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
 
 function getWorkoutData() {
   const apiUrl = "https://www.trainerroad.com/app/api/workoutdetails/";
-  const pageUrl = window.location.href;
-  console.log(pageUrl);
-  const workoutDenominator = pageUrl.replace(
-    "https://www.trainerroad.com/app/cycling/workouts/",
-    ""
-  );
-  console.log(workoutDenominator);
-  const workoutId = workoutDenominator.split("-")[0];
+  const workoutId = getWorkoutId();
+
   fetch(apiUrl + workoutId)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       const { Workout } = data;
       const workoutName = Workout.Details.WorkoutName;
       const { intervalData } = Workout;
       let workoutString = "";
-      // funcion to transform interval data to Zwift workout
       for (let interval of intervalData) {
         if (interval.Name === "Workout") {
           continue;
@@ -40,39 +113,6 @@ function getWorkoutData() {
         workoutString = workoutString.concat(workoutInterval);
       }
 
-      const xml = `
-<workout_file>
-  <author/>
-  <name>${workoutName}</name>
-  <tags>
-    <tag name="TrainerRoad"/>
-  </tags>
-  <description/>
-  <sportType>bike</sportType>
-  <durationType>time</durationType>
-  <tags/>
-  <workout>
-    ${workoutString}
-  </workout>
-</workout_file>
-`;
-
-      console.log(xml);
-
-      const filename = workoutName + ".zwo";
-
-      var element = document.createElement("a");
-      element.setAttribute(
-        "href",
-        "data:text/plain;charset=utf-8," + encodeURIComponent(xml)
-      );
-      element.setAttribute("download", filename);
-
-      element.style.display = "none";
-      document.body.appendChild(element);
-
-      element.click();
-
-      document.body.removeChild(element);
+      exportToXml(workoutName, workoutString);
     });
 }
